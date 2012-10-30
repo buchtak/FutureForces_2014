be30c4537e3cbae8269d4d55a985ba80ca3a3ef6
Josef Vokner <josef.vokner@gmail.com>
2012-10-30 16:14:37 +0100

    activate lang translator
    Zavedení translatoru (knihovny Nette) do BasePresenteru a jeho defaultní
    nastavení v `config.neon` + úprava hlavičky a defaultního nastavení
    `layout.latte`... všechny změny jsou zatím konstrukční a proto nejsou v
    žádné vývojové větvy.

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -6,4 +6,25 @@
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
+	/** @persistent */
+	public $lang;
+
+	/**
+	 * Funkce pro vytvoření šablony v dané jazykové mutaci
+	 * Pokud není konkrétní mutace volána, použije se nastavení z 'config.neon'
+	 */
+	public function createTemplate($class = NULL)
+	{
+		$template = parent::createTemplate($class);
+
+		if (!isset($this->lang))
+		{
+			$this->lang = $this->context->parameters["lang"];
+		}
+
+		$this->context->translator->setLang($this->lang); // nastavíme jazyk
+		$template->setTranslator($this->context->translator);
+
+		return $template;
+	}
 }
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -9,6 +9,7 @@ class HomepagePresenter extends BasePresenter
 	public function renderDefault()
 	{
 		$this->template->anyVariable = 'any value';
+		$this->template->lang = $this->lang;
 	}
 
 }
